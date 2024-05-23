cd9422d1174fff60a2383559ac1d557ae4412597
andychubb <165402941+andychubb@users.noreply.github.com>
2024-05-23 18:03:21 +0100

    First refactor of the conversion method

--- src/app/Utilities/RomanNumerals.php
+++ src/app/Utilities/RomanNumerals.php
@@ -4,22 +4,26 @@ namespace App\Utilities;
 
 class RomanNumerals
 {
+    const NUMERALS = [
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1,
+    ];
+
     public static function convert(int $number): string
     {
-        switch ($number) {
-            case 1:
-                return 'I';
-            case 2:
-                return 'II';
-            case 3:
-                return 'III';
-            case 4:
-                return 'IV';
-            case 5:
-                return 'V';
-            default:
-                return 'Invalid number';
+        $result = '';
+
+        foreach(static::NUMERALS as $numeral => $numeralNumber) {
+            while ($number >= $numeralNumber) {
+                // As long as the number provided is greater than the numeral number then append the numeral to result
+                $result .= $numeral;
+                // Then subtract the numeral number for the next run of the loop
+                $number -= $numeralNumber;
+            }
         }
+
+        return $result;
     }
 }
 
